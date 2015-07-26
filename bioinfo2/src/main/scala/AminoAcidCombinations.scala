3c763f8385af378f22102f44edb86ab06574b42f
Razvan Voicu <razvan.voicu.sg@gmail.com>
2015-07-26 16:14:02 +0800

    Bioinformatics2 - Aminoacid combination count

---
+++ bioinfo2/src/main/scala/AminoAcidCombinations.scala
@@ -0,0 +1,11 @@
+
+object AminoAcidCombinations {
+  def comb : Int => Int = {
+    case 0 => 1
+    case k if k < 0 => 0
+    case k => comb(k-2) + comb(k-3)
+  }
+  def main(args: Array[String]) : Unit = {
+    println(comb(25))
+  }
+}
