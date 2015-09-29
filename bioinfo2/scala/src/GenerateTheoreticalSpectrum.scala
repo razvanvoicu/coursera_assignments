9e60bd6eeed2bf5bdc30edab7f0bf6c308be151b
Razvan Voicu <razvan.voicu.sg@gmail.com>
2015-09-29 20:47:47 +0800

    Bioinformatics 2 - Week 3 - Theoretical Mass Spectrum - Scala.

--- bioinfo2/scala/src/GenerateTheoreticalSpectrum.scala
+++ bioinfo2/scala/src/GenerateTheoreticalSpectrum.scala
@@ -4,7 +4,7 @@ object GenerateTheoreticalSpectrum {
   val massLookup : Map[Char,Int] = Const.mass.toMap
   def main(args: Array[String]) : Unit = {
     val pep = Source.stdin.getLines.toArray.apply(0).toStream
-    val w = pep ++ pep.take(pep.length-1)
+    val w = pep //++ pep.take(pep.length-1)
     val sp = w.tails.flatMap(s => s.reverse.tails.map(_.reverse)).filter(_.length < pep.length).toSet + pep.toStream
     val masses = sp.toStream.map(_.map(massLookup(_)).sum).toArray
     println(masses.sorted.mkString(" "))
