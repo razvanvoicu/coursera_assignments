beff06665fc38b1130b6d351356372e35c36145f
Razvan Voicu <razvan.voicu.sg@gmail.com>
2015-10-10 11:50:48 +0800

    Bioinformatics 4 - Unweighted Pair Group Method with Arithmetic Mean for Ultrametric Trees - Scala

--- bioinfo4/src/main/scala/UPGMA.scala
+++ bioinfo4/src/main/scala/UPGMA.scala
@@ -6,17 +6,65 @@ object UPGMA {
 
   def main(args: Array[String]) : Unit = {
     val in = Source.stdin.getLines()
-    println(upgma(in).mkString(System.getProperty("line.separator")))
+    println(upgma(in).map{ case ((i,j),v) => i + "->" + j + f":$v%.3f" }.mkString(System.getProperty("line.separator")))
+  }
+
+  def closest(d:Map[(Int,Int),Double]) : (Int,Int) = {
+    d.toList.filter{case ((i,j),_) => i != j }.map{case ((i,j),l) => (l,(i,j))}.min._2
+  }
+
+  def removed(d:Map[(Int,Int),Double],p:(Int,Int)) = {
+    d.filter { case ((i,j),_) => i == p._1 || i == p._2 || j == p._1 || j == p._2 }
+  }
+
+  def remove(d:Map[(Int,Int),Double],p:(Int,Int)) = {
+    d.filter { case ((i,j),_) => i != p._1 && i != p._2 && j != p._1 && j != p._2 }
+  }
+
+  def newGraphEdges(q:Map[(Int,Int),Double],k:Int,c:(Int,Int),s:Map[Int,Double],size:Double) : Map[(Int,Int),Double] = {
+    val a = for {
+      ((n1,n2),ln) <- q
+      ((m1,m2),lm) <- q
+      if n1 == c._1 && m1 == c._2 && m2 == n2 && m2 != c._1 && n2 != c._2
+      val d = (ln*s(n1)+lm*s(m1))/size
+      x <- List(((k,n2),d),((n2,k),d))
+    } yield x
+    a + ((k,k) -> 0.0)
+  }
+
+  def treeEdges(a:Map[Int,Double],age:Double,k:Int,c:(Int,Int)) : Map[(Int,Int),Double] = {
+    List(
+      ((k,c._1),age - a(c._1)),
+      ((c._1,k),age - a(c._1)),
+      ((k,c._2),age - a(c._2)),
+      ((c._2,k),age - a(c._2))
+    ).toMap
   }
 
   def upgma(in:Iterator[String]): Map[(Int,Int),Double] = {
     val n = in.next.toInt
     var scn : Scanner = null
-    val distArr = Array.tabulate[Int](n,n) {
-      case (i,j) =>
-        if (j == 0) scn = new Scanner(in.next)
-        scn.nextInt()
+    val distArr = (0 until n).foldLeft(Map[(Int,Int),Double]()) {
+      case (m,i) =>
+        scn = new Scanner(in.next)
+        (0 until n).foldLeft(m) {
+          case (m,j) => m + ((i,j) -> scn.nextDouble)
+        }
     }
-    Map()
+    val ages = (0 until n).map((_,0.0)).toMap
+    val sz = (0 until n).map((_,1.0)).toMap
+    (n until (2*n-1)).foldLeft((ages,sz,distArr,Map[(Int,Int),Double]())) {
+      case ((a,s,d,r),k) =>
+        val c = closest(d)
+        val p = remove(d,c)
+        val q = removed(d,c)
+        val age = (k,d(c)/2)
+        val size = (k -> (s(c._1) + s(c._2)))
+        val edg = newGraphEdges(q,k,c,s,size._2)
+        val te = treeEdges(a,age._2,k,c)
+        (a + age, s+size, p ++ edg,r ++ te)
+    }._4.map{
+      case ((i,j),v) => ((i,j),(v * 100).toInt/100.0)
+    }
   }
 }
--- bioinfo4/src/test/scala/UPGMASmallTest.scala
+++ bioinfo4/src/test/scala/UPGMASmallTest.scala
@@ -2,6 +2,11 @@ import org.scalatest._
 
 class UPGMASmallTest extends FlatSpec with Matchers {
 
+  def mapEq(a:Map[(Int,Int),Double],b:Map[(Int,Int),Double]) : Boolean = {
+    (a.keys.toSet == b.keys.toSet) &&
+      (a.forall{case ((i,j),v) => Math.abs(v - b((i,j))) < 0.0001})
+  }
+
   it should "pass the test" in {
     val inp =
       """4
@@ -12,7 +17,7 @@ class UPGMASmallTest extends FlatSpec with Matchers {
 
     val resStr =
       """0->5:7.000
-        |1->6:8.833
+        |1->6:8.833333333333334
         |2->4:5.000
         |3->4:5.000
         |4->2:5.000
@@ -20,15 +25,15 @@ class UPGMASmallTest extends FlatSpec with Matchers {
         |4->5:2.000
         |5->0:7.000
         |5->4:2.000
-        |5->6:1.833
-        |6->5:1.833
-        |6->1:8.833""".stripMargin.lines
+        |5->6:1.833333333333334
+        |6->5:1.833333333333334
+        |6->1:8.833333333333334""".stripMargin.lines
 
     val res =
       ( resStr
           .map(_.split("->|:") match {case Array(s1,s2,s3) => ((s1.toInt,s2.toInt) -> s3.toDouble)})
           .toMap )
-    UPGMA.upgma(inp.lines) should be (res)
+    mapEq(UPGMA.upgma(inp.lines), res)
   }
 
 }
