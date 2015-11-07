c2ec3ba3fadb2c6b04b37abe7c26a84b04d922ae
Razvan Voicu <razvan.voicu.sg@gmail.com>
2015-11-07 21:09:54 +0800

    BioInfo4 - Small Parsimony solved - Scala.

--- bioinfo4/src/main/scala/SmallParsimony.scala
+++ bioinfo4/src/main/scala/SmallParsimony.scala
@@ -1,6 +1,12 @@
-import scala.util.{Success, Try}
+import scala.io.Source
+import scala.util.{Failure, Success, Try}
 
 object SmallParsimony {
+  def main(args: Array[String]) : Unit = {
+    val lines = Source.stdin.getLines.toArray
+    println(smallParsimony(lines))
+  }
+
   val nucl = Map(
     'A' -> Long.MaxValue/2,
     'C' -> Long.MaxValue/2,
@@ -8,33 +14,39 @@ object SmallParsimony {
     'T' -> Long.MaxValue/2)
   def smallParsimony(lines: Array[String]) : String = {
     val leaves = lines(0).toInt
-    val (dnaLgth,_,tree) = lines.tail.foldLeft((0,0,Map[Int,List[(Int,Array[Map[Char,Long]],Array[Char])]]())) {
+    val (dnaLgth,_,tree) = lines.tail.foldLeft((0,0,Map[Int,List[(Int,Array[Map[Char,Long]])]]())) {
       case ((dnaLgth,nodeCnt,m),s) =>
         val edge = s.split("->")
-        val dl = Math.max(edge(1).length,dnaLgth)
+        val srcTry = Try(edge(0).toInt)
         val destTry = Try(edge(1).toInt)
-        val destInt = destTry.getOrElse(nodeCnt)
-        val destMap = destTry.transform(
-          _ => Success(Array.fill(dnaLgth)(nucl)),
-          _ => Success(edge(1).toCharArray.map { c => nucl + (c -> 0L) })
-        ).get
-        val add = if (destTry.isFailure) 1 else 0
-        val k = edge(0).toInt
-        val existing = m.getOrElse(k,List())
-        (dl, nodeCnt + add, m + ((k -> (existing :+ ((destInt,destMap,Array.fill(dl)(' ')))))))
+        srcTry match {
+          case Success(src) if destTry.isInstanceOf[Failure[Int]] || src > destTry.get =>
+            val destInt = destTry.getOrElse (nodeCnt)
+            val destMap = destTry.transform (
+            _ => Success (Array.fill (dnaLgth) (nucl) ),
+            _ => Success (edge (1).toCharArray.map {
+            c => nucl + (c -> 0L)
+            })
+            ).get
+            val add = if (destTry.isFailure) 1 else 0
+            val k = edge (0).toInt
+            val existing = m.getOrElse (k, List () )
+            (Math.max (edge (1).length, dnaLgth), nodeCnt + add, m + ((k -> (existing :+ ((destInt, destMap) ) ) ) ) )
+          case _ => (dnaLgth,nodeCnt,m)
+        }
     }
     val traversable = tree + ((-1 -> List((tree.keys.max,Array.fill(dnaLgth)(nucl)))))
     traverse(dnaLgth, traversable, tree.keys.max,-1,0)
     val parsimony = traversable(-1)(0)._2.map{_.map{case (k,v) => v}.min}.sum
-    parsimony + "\n" + adjacency(traversable,tree.keys.max,-1,0)
+    parsimony + "\n" + adjacency(traversable,tree.keys.max,-1,0,dnaAsString(traversable(-1)(0)._2, " " * dnaLgth))
   }
 
-  def dnaAsString(a: Array[Map[Char,Long]]) =
-    a.map{ _.map{case (k,v) => (v,k)}.min._2}.mkString("")
+  def dnaAsString(a: Array[Map[Char,Long]], parent: String) =
+    a.zip(parent).map{ case (m,c) => m.map{case (k,v) => (v + {if(k==c) 0 else 1},k)}.min._2}.mkString("")
   def hamming(s1: String, s2: String) =
     s1.zip(s2).map{case (c1,c2) => if (c1==c2) 0 else 1}.sum
 
-  def traverse(dnaLgth: Int, tree: Map[Int,List[(Int,Array[Map[Char,Long]],Array[Char])]], node: Int, parent: Int, child: Int) : Unit = {
+  def traverse(dnaLgth: Int, tree: Map[Int,List[(Int,Array[Map[Char,Long]])]], node: Int, parent: Int, child: Int) : Unit = {
     if (tree.isDefinedAt(node)) {
       val left = tree(node)(0)
       val leftIdx = left._1
@@ -51,18 +63,18 @@ object SmallParsimony {
     }
   }
 
-  def adjacency(tree: Map[Int,List[(Int,Array[Map[Char,Long]])]], node: Int, parent: Int, child: Int) : String = {
+  def adjacency(tree: Map[Int,List[(Int,Array[Map[Char,Long]])]], node: Int, parent: Int, child: Int, ps: String) : String = {
     if (tree.isDefinedAt(node)) {
       val left = tree(node)(0)
       val leftIdx = left._1
-      val leftDna = dnaAsString(left._2)
+      val leftDna = dnaAsString(left._2,ps)
       val right = tree(node)(1)
       val rightIdx = right._1
-      val rightDna = dnaAsString(right._2)
-      val parentDna = dnaAsString(tree(parent)(child)._2)
+      val rightDna = dnaAsString(right._2,ps)
+      val parentDna = ps
       val hl = hamming(leftDna,parentDna)
       val hr = hamming(rightDna,parentDna)
-      adjacency(tree, leftIdx, node, 0) + adjacency(tree, rightIdx, node, 1) +
+      adjacency(tree, leftIdx, node, 0, leftDna) + adjacency(tree, rightIdx, node, 1, rightDna) +
         s"$parentDna->$leftDna:$hl" + "\n" + s"$parentDna->$rightDna:$hr" + "\n" +
         s"$leftDna->$parentDna:$hl" + "\n" + s"$rightDna->$parentDna:$hr" + "\n"
     } else ""
--- bioinfo4/src/test/scala/SmallParsimonySmallTest.scala
+++ bioinfo4/src/test/scala/SmallParsimonySmallTest.scala
@@ -14,21 +14,22 @@ class SmallParsimonySmallTest extends FlatSpec with Matchers {
 
   val testExpectedOutput =
   """16
-    |ATTGCGAC->ATAGCCAC:2
-    |ATAGACAA->ATAGCCAC:2
-    |ATAGACAA->ATGGACTA:2
-    |ATGGACGA->ATGGACTA:1
-    |CTGCGCTG->ATGGACTA:4
-    |ATGGACTA->CTGCGCTG:4
-    |ATGGACTA->ATGGACGA:1
-    |ATGGACTA->ATAGACAA:2
-    |ATAGCCAC->CAAATCCC:5
-    |ATAGCCAC->ATTGCGAC:2
-    |ATAGCCAC->ATAGACAA:2
-    |CAAATCCC->ATAGCCAC:5""".stripMargin
+    |CTTGTCTC->CAAATCCC:4
+    |CTTGTCTC->ATTGCGAC:4
+    |CAAATCCC->CTTGTCTC:4
+    |ATTGCGAC->CTTGTCTC:4
+    |CTGGTCTG->CTGCGCTG:2
+    |CTGGTCTG->ATGGACGA:4
+    |CTGCGCTG->CTGGTCTG:2
+    |ATGGACGA->CTGGTCTG:4
+    |CTTGTCTG->CTTGTCTC:1
+    |CTTGTCTG->CTGGTCTG:1
+    |CTTGTCTC->CTTGTCTG:1
+    |CTGGTCTG->CTTGTCTG:1
+    |""".stripMargin
 
   it should "pass the test" in {
-    println(smallParsimony(testInput.split(System.getProperty("line.separator"))))
+    println("===" + smallParsimony(testInput.split(System.getProperty("line.separator"))) + "===")
     smallParsimony(testInput.split(System.getProperty("line.separator"))) shouldBe testExpectedOutput
   }
 }
