cdb37db7e0eaf25742b6d9a54f00ab29622c50d9
Razvan Voicu <razvan.voicu.sg@gmail.com>
2015-11-07 11:44:39 +0800

    Bioinfo 4 - Small Parsimony: added tree traversal - Scala.

--- bioinfo4/src/main/scala/SmallParsimony.scala
+++ bioinfo4/src/main/scala/SmallParsimony.scala
@@ -7,38 +7,60 @@ object SmallParsimony {
     'G' -> Long.MaxValue,
     'T' -> Long.MaxValue)
   def smallParsimony(lines: Array[String]) : String = {
-    val n = lines(0).toInt
-    val (lgth,_,tree) = lines.tail.foldLeft((0,0,Map[Int,List[(Int,Array[Map[Char,Long]])]]())) {
-      case ((lgth,cnt,m),s) =>
+    val leaves = lines(0).toInt
+    val (dnaLgth,_,tree) = lines.tail.foldLeft((0,0,Map[Int,List[(Int,Array[Map[Char,Long]])]]())) {
+      case ((dnaLgth,nodeCnt,m),s) =>
         val edge = s.split("->")
         val destTry = Try(edge(1).toInt)
-        val destInt = destTry.getOrElse(cnt)
-        val destStr = destTry.transform(_ => Success(""), _ => Success(edge(1))).get
+        val destInt = destTry.getOrElse(nodeCnt)
+        val destMap = destTry.transform(
+          _ => Success(Array.fill(dnaLgth)(nucl)),
+          _ => Success(edge(1).toCharArray.map { c => nucl + (c -> 0L) })
+        ).get
         val add = if (destTry.isFailure) 1 else 0
         val k = edge(0).toInt
         val existing = m.getOrElse(k,List())
-        (lgth + 1, cnt + add, m + ((k,existing :+ ((destInt,destStr.toCharArray.map { c => nucl + (c -> 0L) })))))
+        (Math.max(edge(1).length,dnaLgth), nodeCnt + add, m + ((k -> (existing :+ ((destInt,destMap))))))
     }
+    val traversable = tree + ((-1 -> List((tree.keys.max,Array.fill(dnaLgth)(nucl)))))
+    traverse(dnaLgth, traversable, tree.keys.max,-1,0)
     tree.foreach{
       case (k,a) =>
         println(s"k=$k")
         a.foreach{
           l =>
-          println(s"[[[${l._1}")
-          l._2.foreach{ println(_)}
-          println("]]]")
+            println(s"[[[${l._1}")
+            l._2.foreach{ println(_)}
+            println("]]]")
         }
     }
-    sealed trait Node
-    case class Leaf(val key: Int, val dna: Array[Map[Char,Long]]) extends Node
-    case class Inner(val key:Int, val dna: Array[Map[Char,Long]], val left: Node, val right: Node) extends Node
-
-/*    val nodes = (0 until lgth).map {
-      k => tree.get(k) match {
-        case None => (k,Leaf(k,Array()))
-        case Some(i,m) => (k,)
-      }
-    } */
     ""
+  }
+
+  def traverse(dnaLgth: Int, tree: Map[Int,List[(Int,Array[Map[Char,Long]])]], node: Int, parent: Int, child: Int) : Unit = {
+    if (tree.isDefinedAt(node)) {
+      val left = tree(node)(0)
+      val leftIdx = left._1
+      val leftDna = left._2
+      traverse(dnaLgth,tree,leftIdx,node,0)
+      val right = tree(node)(1)
+      val rightIdx = left._1
+      val rightDna = right._2
+      traverse(dnaLgth,tree,rightIdx,node,1)
+      (0 until dnaLgth).foreach {
+        i =>
+          tree(parent)(child)._2(i) = computeDna(leftDna(i),rightDna(i))
+      }
+    }
+  }
+
+  def computeDna(left: Map[Char,Long], right: Map[Char,Long]) : Map[Char,Long] = {
+    val (minLeftV,minLeftK) = left.map{case (k,v) => (v,k)}.min
+    val (minRightV,minRightK) = right.map{case (k,v) => (v,k)}.min
+    def pick(k: Char) = Math.min(
+      { if (k == minLeftK) minLeftV else minLeftV+1 },
+      { if (k == minRightK) minRightV else minRightV+1 }
+    )
+    left.map{ case (k,_) => (k, pick(k)) }
   }
 }
