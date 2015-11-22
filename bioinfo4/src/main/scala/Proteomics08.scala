6dcb8af366c4a8eaa68d0bd37e191a4cc071133f
Razvan Voicu <razvan.voicu.sg@gmail.com>
2015-11-22 20:24:15 +0800

    Bioinformatics 4 - Proteomics Code Challenge 8 - Scala.

---
+++ bioinfo4/src/main/scala/Proteomics08.scala
@@ -0,0 +1,70 @@
+import java.util.Scanner
+
+import scala.collection.mutable
+import scala.io.Source
+
+object Proteomics08 {
+
+  val masses = Map[Char,Int](
+    'Q' -> 128,
+    'K' -> 128,
+    'P' -> 97,
+    'V' -> 99,
+    'M' -> 131,
+    'T' -> 101,
+    'A' -> 71,
+    'E' -> 129,
+    'H' -> 137,
+    'C' -> 103,
+    'I' -> 113,
+    'L' -> 113,
+    'N' -> 114,
+    'D' -> 115,
+    'F' -> 147,
+    'S' -> 87,
+    'G' -> 57,
+    'Y' -> 163,
+    'R' -> 156,
+    'W' -> 186
+    /*'X' → 4,
+    'Z' → 5*/
+  )
+
+  val cache = mutable.Map[(Int,Int),Int]()
+
+  def size(spectrum: Array[Int], maxScore: Int, idx: Int, t: Int): Int = {
+    (idx,t) match {
+      case (0,0) ⇒ 1
+      case (0,_) ⇒ 0
+      case (idx,_) if idx < 0 ⇒ 0
+      case (_,t) if t < 0 ⇒ 0
+      case (idx,t) ⇒ masses.values.toList.map {
+        v:Int ⇒ cache.get((idx-v,t-spectrum(idx))) match {
+          case None ⇒
+            val sz = size(spectrum, maxScore, idx-v, t-spectrum(idx))
+            cache += (idx-v,t-spectrum(idx)) -> sz
+            sz
+          case Some(sz) ⇒ sz
+        }
+      }.sum
+    }
+  }
+
+  def main(args: Array[String]) : Unit = {
+    val lines = Source.stdin.getLines.toArray
+    val spectrumLine = lines(0)
+    val sc = new Scanner(spectrumLine)
+    val spectrum = (0 #:: Stream.continually(if(sc.hasNextInt) sc.nextInt else Int.MinValue)
+      .takeWhile(_ != Int.MinValue)).toArray
+    val threshold = lines(1).toInt
+    val maxScore = 2 * lines(2).toInt
+    println("Spectrum:")
+    println(spectrum.mkString(" "))
+    println(s"Spectrum size: ${spectrum.size}")
+    println(s"Threshold: $threshold")
+    println(s"MaxScore: $maxScore")
+    println
+    val result = (threshold to maxScore).map(t ⇒ size(spectrum, maxScore, spectrum.size-1,t)).sum
+    println(result)
+  }
+}
